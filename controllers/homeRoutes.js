fc543ba520cca0a0cf7b579a1f2ae3514128cc69
DerTodd <88950531+DerTodd@users.noreply.github.com>
2022-01-18 17:04:49 -0600

    creating homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,39 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const router = require("express").Router();
+const { Category, Product } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: Category,
+    });
+
+    const products = productData.map((post) => post.get({ plain: true }));
+    console.log(products);
+    res.render("homepage", {
+      products: products,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/product/:id', async (req, res) => {
+    try {
+      const productData = await Product.findByPk(req.params.id, {
+    
+        include: [
+          Category,
+        ]
+  
+      });
+      
+      const product = productData.get({ plain: true });
+      console.log(product)
+      res.render('product', {
+        ...product,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
